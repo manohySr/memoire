0a92022dadc77b3afd1bda4af11adea699c53907
Manohy Saotra RAJAONAH <86122918+manohySr@users.noreply.github.com>
2022-04-19 19:33:45 +0100

    Init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import {BrowserRouter as Routers, Routes, Route, Link} from "react-router-dom";
+import Home from '.components/Home';
+import About from '.components/About';
+import Profile from '.components/Profile';
+
 
 const App = () => {
     return (
-        <div>
-            HELLO WORLD
-        </div>
+        <>
+        <Routers>
+            <nav>
+                <link to="/">Home</link>
+                <link to="/about">About</link>
+                <link to="/profile">Profile</link>
+            </nav>
+            <Routes>
+                <Route path='/' element={<Home />}></Route>
+                <Route path='/about' element={<About />}></Route>
+                <Route path='/profile' element={<Profile />}></Route>
+            </Routes>
+        </Routers>
+        
+        </>
     );
 }
 
