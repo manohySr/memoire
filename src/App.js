2ec827e553ffdcec4120087c467304cef99c591f
manohySr <manohyrajaonah@gmail.com>
2022-04-25 22:45:04 +0100

    front memoire starting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,6 @@
-import React from 'react';
-import {BrowserRouter as Routers, Routes, Route} from "react-router-dom";
-import Home from './components/Home';
-import About from './components/About';
-import Profile from './components/Profile';
-import Navbar from './components/Navbar';
-
-
 const App = () => {
     return (
-        <>
-        <Routers>
-            <Navbar />
-            <Routes>
-                <Route path='/' element={<Home />}></Route>
-                <Route path='/about' element={<About />}></Route>
-                <Route path='/profile' element={<Profile />}></Route>
-            </Routes>
-        </Routers>
-
-        
-        </>
+        <div></div>
     );
 }
 
